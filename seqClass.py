d5bcb9109d43678c35b9546058288463152cda87
ManonGeerts-UVIC <manon.geerts@uvic.cat>
2021-03-05 14:29:21 +0100

    Exercise 1: change fix comment

--- seqClass.py
+++ seqClass.py
@@ -13,7 +13,7 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
-args.seq = args.seq.upper()                 # Note we just added this line
+args.seq = args.seq.upper()                 # Note we just added this line, this will fix
 
 ### classify sequence
 
